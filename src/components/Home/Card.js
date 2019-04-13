d54211bf4b02845fa6deb4ea9a1ade83f727ec18
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-04-13 16:42:26 -0300

    font-awesome included

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import FontAwesome from 'react-fontawesome'
+import cx from 'classnames'
 
 class Card extends Component {
   render() {
-    const { image, title, description, buttonText } = this.props
+    const { image, title, description, buttonText, className } = this.props
 
     return (
       <div className="col-lg-4 col-md-4 col-sm-6 col-xs-12">
         <div className="card bg-primary">
-          <div className="card-img-top text-center">
-            <img className="m-auto" src={image} />
+          <div className="card-img-top">
+            <FontAwesome name={image} className={cx('m-auto', className)} />
           </div>
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
@@ -27,6 +29,7 @@ Card.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
+  className: PropTypes.string
 }
 
 export default Card
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Loading from '../Loading'
 import moment from 'moment'
-import Card from './Card';
+import Card from './Card'
 
 class Home extends Component {
   constructor(props) {
@@ -34,11 +34,11 @@ class Home extends Component {
 
     return (
       <div id="home" className="container">
-        <h6>{moment().format('dddd, MMMM Do YYYY')}</h6>
+        <h6 className="text-right">{moment().format('dddd, MMMM Do YYYY')}</h6>
         <div className="row">
-          <Card image="/new-user.png" title="Novo Beneficiário" description="Adicionar um novo beneficiário que pode receber transferências." buttonText="Adicionar"/>
-          <Card image="/transfer-icon.png" title="Fazer Transferência" description="Faça uma transferência para um de seus beneficiários." buttonText="Transferir"/>
-          <Card image="/history-icon.png" title="Ver Histórico" description="Veja o histórico de transferências efetuadas para seus beneficiários." buttonText="Ver"/>
+          <Card image="users" title="Novo Beneficiário" description="Adicionar um novo beneficiário que pode receber transferências." buttonText="Adicionar" className="fa-5x"/>
+          <Card image="money" title="Fazer Transferência" description="Faça uma transferência para um de seus beneficiários." buttonText="Transferir" className="fa-5x"/>
+          <Card image="history" title="Ver Histórico" description="Veja o histórico das transações efetuadas para os seus beneficiários." buttonText="Ver" className="fa-5x"/>
         </div>
       </div>
     )
